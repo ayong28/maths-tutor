1dd6f6f17c9b197b47c42a22a589d7664b1bc16f
ayong28 <adrianyong3@gmail.com>
2026-01-01 17:58:30 +1100

    Update vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -19,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
