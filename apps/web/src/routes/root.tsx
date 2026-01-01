d3d20470feed42dfe77f38edb5ae52602b17cbfa
ayong28 <adrianyong3@gmail.com>
2026-01-01 20:56:24 +1100

    Simplify main page

--- apps/web/src/routes/root.tsx
+++ apps/web/src/routes/root.tsx
@@ -1,21 +1,8 @@
-import { Outlet, Link } from 'react-router';
+import { Outlet  } from 'react-router';
 
 export default function Root() {
   return (
-    <div className="min-h-screen bg-linear-to-r from-blue-600 to-blue-400 flex flex-col items-center py-8 px-2 print:bg-white print:text-black">
-      {/* Header */}
-      <header className="mb-8 text-center print:hidden">
-        <Link to="/" className="hover:opacity-80 transition">
-          <h1 className="text-3xl font-bold text-blue-950 mb-2">
-            Maths Tutoring Worksheets
-          </h1>
-          <p className="text-blue-900">
-            Select a topic and category to view printable worksheets.
-          </p>
-        </Link>
-      </header>
-
-      {/* Route content renders here */}
+    <div className="min-h-screen bg-linear-to-r bg-gray-500 flex flex-col items-center py-8 px-2 print:bg-white print:text-black">
       <Outlet />
     </div>
   );
